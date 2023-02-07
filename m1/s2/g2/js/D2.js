06df86bd7c6e01170e3820c50dacbb82f43943fe
Marianna <mari.albrici@me.com>
2023-02-07 15:05:08 +0100

    Day 2 - esercizio

--- m1/s2/g2/js/D2.js
+++ m1/s2/g2/js/D2.js
@@ -12,36 +12,41 @@ REGOLE
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA 
-String: rappresenta del testo. Possono essere anche SVGAnimatedNumberList, ma non hanno valore.
-Number: indica dei numeri con i rispettivi valori
+String: rappresenta del testo. Possono essere anche numeri, ma non hanno un valore. Viene riconosciuto perchè si trova all'interno di apici singoli o doppi, come per esempio 'ciao' o "ciao45"
+Number: indica dei numeri con il rispettivo valore, dando errore nel caso ci sia del testo presente. Non ha bisogno di apici o virgolette per essere riconosciuto
 Boolean: dato che può essere solo vero o falso
-undefined
-null
+Undefined: dato che non è stato definito, non gli è stato dato un valore (di testo o numero)
+Null: dato che intenzionalmente non esiste 
+Object: un insieme di dati collegati tra loro (tramite l'operatore :)
 */
 
 /* ESERCIZIO 2
  Crea una variable chiamata "name" e assegna ad essa il tuo nome, sotto forma di stringa.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA 
-var name = 'Marianna'
-*/
+/* SCRIVI QUI LA TUA RISPOSTA */
+let nome = 'Marianna';
+console.log(nome); 
+
 
 /* ESERCIZIO 3
  Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA 
-console.log(12 + 20)
-*/
+/* SCRIVI QUI LA TUA RISPOSTA */
+let a = 12;
+let b = 20;
+console.log(a + b);
+
 
 /* ESERCIZIO 4
  Crea una variable di nome "x" e assegna ad essa il numero 12.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA 
-var x = 12
-*/
+/* SCRIVI QUI LA TUA RISPOSTA */
+let x = 12;
+console.log(x);
+
 
 /* ESERCIZIO 5
   Riassegna un nuovo valore alla variabile "name" già esistente: il tuo cognome.
@@ -49,12 +54,22 @@ var x = 12
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+nome = 'Albrici'
+console.log(nome);
+
+/* const SECONDONOME ='Marianna';
+console.log(SECONDONOME);
+
+const SECONDONOME ='Albrici';
+console.log(SECONDONOME); */ 
+
 
 /* ESERCIZIO 6
  Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+console.log(4 - x);
 
 /* ESERCIZIO 7
  Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
@@ -63,3 +78,8 @@ var x = 12
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+let name1 = 'john';
+let name2 = 'John';
+console.log('john' != 'John'); // oppure console.log('john' !== 'John') - viene comparato comuqnue il valore del dato, essendo dello stesso tipo 
+
+console.log(name1.toLowerCase() === name2.toLowerCase());
