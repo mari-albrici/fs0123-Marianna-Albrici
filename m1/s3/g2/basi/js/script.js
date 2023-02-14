b368eb22a3e72036bdc242014b1bccca4403e7ba
Marianna <mari.albrici@me.com>
2023-02-14 16:45:33 +0100

    Day 2 - Esercizio

---
+++ m1/s3/g2/basi/js/script.js
@@ -0,0 +1,5 @@
+// Array = variabile capace di memorizzare una serie di valori anzichè uno solo alla volta. Non hanno coppie di chiavi-valore, ma singoli valore
+
+//indici -> iniziano da 0, non da 1
+//typeof -> indica il tipo di dato 
+
